057e1b0f407e1721f70d9b9bb19127c2f679acb0
mdhasan-999 <webdeveloperhasan999@gmail.com>
2022-05-23 16:30:26 +0600

    Dashboard Setup

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
+        });
+
+        // See Your Order 
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const orders = await orderCollection.find(query).toArray();
+            res.send(orders);
         })
     }
     finally {
